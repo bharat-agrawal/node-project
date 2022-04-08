6de9bc5ad0ebeed6eeab256af3758d88d9b03456
bharat <bharat.agrawal@sarvika.com>
2022-04-08 16:53:38 +0530

    field updates

--- services/users.service.js
+++ services/users.service.js
@@ -8,41 +8,59 @@ const getAll = function (req, res) {
 };
 
 const getOne = function (req, res) {
-  connection.query(`select * from employee WHERE id=${req.params.id}`, function (error, results, fields) {
-    if (error) throw error;
-    console.log("The response is: ", results);
-    res.send(results);
-  });
+  connection.query(
+    `select * from employee WHERE id=${req.params.id}`,
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log("The response is: ", results);
+      res.send(results);
+    }
+  );
 };
 
 const updateOne = function (req, res) {
   const name = req.body.employee_name;
   const salary = req.body.employee_salary;
   const age = req.body.employee_age;
-  connection.query(`UPDATE employee SET employee_name='${name}',employee_salary=${salary},employee_age=${age} where id=${req.params.id}`, function (error, results, fields) {
-    if (error) throw error;
-    console.log("The response is: ", results);
-    res.send(results);
-  });
+  const code = req.body.employee_code;
+  const department = req.body.employee_department;
+  const designation = req.body.employee_designation;
+  connection.query(
+    `UPDATE employee SET employee_name='${name}',employee_salary=${salary},employee_age=${age},employee_code=${code},employee_department=${department},employee_designation=${designation} where id=${req.params.id}`,
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log("The response is: ", results);
+      res.send(results);
+    }
+  );
 };
 
 const deleteOne = function (req, res) {
-  connection.query(`DELETE from employee where id=${req.params.id}`, function (error, results, fields) {
-    if (error) throw error;
-    console.log("The response is: ", results);
-    res.send(results);
-  });
+  connection.query(
+    `DELETE from employee where id=${req.params.id}`,
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log("The response is: ", results);
+      res.send(results);
+    }
+  );
 };
 
 const createOne = function (req, res) {
   const name = req.body.employee_name;
   const salary = req.body.employee_salary;
   const age = req.body.employee_age;
-  connection.query(`INSERT INTO employee (employee_name, employee_salary, employee_age) VALUES ('${name}',${salary},${age})`, function (error, results, fields) {
-    if (error) throw error;
-    console.log("The response is: ", results);
-    res.send(results);
-  });
+  const code = req.body.employee_code;
+  const department = req.body.employee_department;
+  const designation = req.body.employee_designation;
+  connection.query(
+    `INSERT INTO employee (employee_name, employee_salary, employee_age, employee_code, employee_department, employee_designation) VALUES ('${name}',${salary},${age},${code},${department},${designation})`,
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log("The response is: ", results);
+      res.send(results);
+    }
+  );
 };
 
 module.exports = {
@@ -50,5 +68,5 @@ module.exports = {
   getOne,
   updateOne,
   deleteOne,
-  createOne
+  createOne,
 };
